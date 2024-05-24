e6cbe079ed56cdbfa3318ac2376eb08e2db76e12
MCyssa <mariacecilia.mvh@gmail.com>
2024-05-24 18:54:57 -0300

    Primeiro commit

---
+++ aula1.py
@@ -0,0 +1,48 @@
+"""Fundamentos em python
+Variáveis, prints e scripts simples"""
+
+#script simples
+print ("Hello, world!")
+
+# Variáveis
+mensagem = "Esta é a primeira aula em python!"
+numero = 1000
+pi = 3,14159
+
+# Imprimindo variáveis
+
+print (mensagem)
+print (numero)
+print (pi)
+
+#exercício
+
+nome = "Maria"
+idade = 20 
+cidade = "Barra do Ribeiro"
+
+print (f"Nome: {nome}")
+print (f"Idade: {idade}")
+print (f"Cidade: {cidade}")
+
+# operações básicas
+num1 = int(input("digite um número: "))
+num2 = int(input("digite outro numero: "))
+
+soma = num1 + num2
+sub = num1 - num2
+multi = num1 * num2
+div = num1 / num2
+
+print (soma)
+print (sub)
+print (multi)
+print (div)
+
+# concatenando strings
+str1 = "Olá"
+str2 = "Mundo"
+
+resultado = str1 + " " + str2
+print (resultado)
+
