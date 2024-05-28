a5379acc51b3347a2e4e32e13aa554b040ba2291
MCyssa <mariacecilia.mvh@gmail.com>
2024-05-28 17:39:32 -0300

    Funções

--- dia_4.py
+++ dia_4.py
@@ -1,3 +1,5 @@
+# Gerador de senhas
+print("Gerador de senhas")
 import random 
 import string 
 
@@ -10,3 +12,32 @@ tamanho = int(input("Digite o tamanho da senha: "))
 
 senha = gerar_senha(tamanho)
 print(f"A senha gerada é: {senha}")
+
+# Calculador de fatorial
+print("Iniciando fatoração")
+def fatorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        fatorial = 1 
+        for i in range(2, n + 1):
+            fatorial *= i
+        return fatorial 
+
+num = int(input('Digite um número:'))
+print(f"O fatorial de {num} é {fatorial(num)}")
+
+# Para calcular a média dos números
+print("Programa de fatoração concluído! Iniciando o cálculo de média.")
+def calcular_media(numeros):
+    soma = sum(numeros)
+    quantidade = len(numeros)
+    media = soma / quantidade
+    return media
+
+lista = input("Digite uma lista de números: ")
+numeros = [int(x) for x in lista.split()]
+
+media = calcular_media(numeros)
+
+print(f"Média dos numeros: {media}")
