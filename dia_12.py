44599793ae47d42b5a36fc533db6a16542b6d742
MCyssa <mariacecilia.mvh@gmail.com>
2024-06-15 15:11:34 -0300

    Gerenciamento de Tarefas

--- dia_12.py
+++ dia_12.py
@@ -31,7 +31,7 @@ def removerTarefa(tarefa_index):
 
 def listarTarefas():
     if not tarefas:
-        print("Nenhuma tarefa foi adicionada.")
+        print("O arquivo está vazio.")
         return
     
     for index, tarefa in enumerate(tarefas):
